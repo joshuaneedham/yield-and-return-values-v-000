e45320ad2e2415e2db215eef6bd6632ae0a34600
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 13:47:27 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+binding.pry
 def hello(array)
   i = 0
   while i < array.length
@@ -8,5 +9,5 @@ def hello(array)
   end
 end
 
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
